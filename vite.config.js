5d119360fb350ca73347a9f05676f3f1a4a51edf
Aruna <aruna.evan15@gmail.com>
2023-06-19 18:12:07 -0700

    fix: move env variable to repo variable for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // this base is specific to silly-cv, change if necessary
   base: '/silly-cv/',
 })
